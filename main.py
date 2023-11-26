9e8a3ab459ebc1f81ac8644a7f8c54ae81271f7b
PasqualePedoto <pasquale.pedoto123@gmail.com>
2023-11-26 19:24:04 +0100

    Add mail sent feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = FastAPI()
 
@@ -9,3 +10,29 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/sendEmail")
+def sendEmail():
+    # Imposta le tue credenziali
+    email_address = 'pasquale.pedoto123@gmail.com'
+    password = 'hzldjaotjvutdrbi'
+
+    # Crea il messaggio
+    subject = 'Oggetto della mail'
+    body = 'Corpo del messaggio'
+
+    message = MIMEMultipart()
+    message['From'] = email_address
+    message['To'] = 'ivanop92@gmail.com'
+    message['Subject'] = subject
+
+    message.attach(MIMEText(body, 'plain'))
+
+    # Connessione al server SMTP di Gmail
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()  # Avvia la connessione TLS
+        server.login(email_address, password)
+        server.send_message(message)
+
+    return {
+        "status": "Email inviata"
+    }
